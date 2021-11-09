a2d737747b97ba63d52b41fe5ed8eec0b17e1a51
Genne Liu <genneliu@gmail.com>
2021-11-09 10:35:19 -0800

    reducers

--- src/Components/Budget/Budget.js
+++ src/Components/Budget/Budget.js
@@ -8,23 +8,39 @@ import Loading from "./../shared/Loading/Loading";
 import Nav from "./../shared/Nav";
 import "./Budget.css";
 import { connect } from "react-redux";
+import { requestUserData } from "./../../redux/userReducer";
+import {
+  requestBudgetData,
+  addPurchase,
+  removePurchase,
+} from "./../../redux/budgetReducer";
 
 class Budget extends Component {
+  componentDidMount() {
+    //action creatr is invoked
+    this.props.requestUserData();
+    this.props.requestBudgetData();
+  }
+
   render() {
-    const { loading } = this.props.budget;
+    const { loading, purchases, budgetLimit } = this.props.budget;
+    const { firstName, lastName } = this.props.user;
     return (
       <Background>
         {loading ? <Loading /> : null}
         <div className="budget-container">
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName} />
           <div className="content-container">
             <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases />
+              <AddPurchase addPurchase={this.props.addPurchase} />
+              <DisplayPurchases
+                purchases={purchases}
+                removePurchase={this.props.removePurchase}
+              />
             </div>
             <div className="chart-container">
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit} />
+              <Chart2 purchases={purchases} />
             </div>
           </div>
         </div>
@@ -35,7 +51,13 @@ class Budget extends Component {
 function mapStateToProps(state) {
   return {
     budget: state.budget,
+    user: state.user,
   };
 }
 
-export default connect(mapStateToProps)(Budget);
+export default connect(mapStateToProps, {
+  requestUserData,
+  requestBudgetData,
+  addPurchase,
+  removePurchase,
+})(Budget);
--- src/redux/userReducer.js
+++ src/redux/userReducer.js
@@ -1,9 +1,33 @@
+import axios from "axios";
+
 const initialState = {
   email: null,
   firstName: null,
   lastName: null,
 };
 
-export default function userReducer(state = initialState, action) {
-  return state;
+//action type
+const REQUEST_USER_DATA = "REQUEST_USER_DATA";
+
+// action creator
+export const requestUserData = () => {
+  let data = axios.get("/auth/user-data").then((res) => res.data);
+  return {
+    type: REQUEST_USER_DATA,
+    payload: data,
+  };
+};
+
+export default function reducer(state = initialState, action) {
+  // USING A SWITCH STATEMENT INSIDE OF THIS FUNCTION ENABLES THE REDUX STORE TO UPDATE ITS STATE DYNAMICALLY BASED ON THE ACTION TYPE PASSED IN. THIS FUNCTION RUNS WHEN THE ACTION CREATORS ARE INVOKED IN A COMPONENT.
+
+  // NOTE: YOU USE THE PROMISE BASED SYNTAX '_FULFILLED' BECAUSE 'FULFILLED'/'PENDING'/'REJECTED' IS ADDED BY REDUX-PROMISE  MIDDLEWARE WHEN MAKING PROMISE BASED REQUESTS (REQUESTS THAT RECEIVE RESPONSES) THROUGH REDUX. ACTIONS THAT AREN'T RELATED TO PPROMISE BASED REQUESTS SHOULD NOT HAVE 'FULFILLED'/'PENDING'/'REJECTED' CONCATENATED TO THEM. IF YOU DIDN'T USE REDUX-PROMISE-MIDDLEWARE, YOU SHOULD ALSO NOT CONCATENATE THOSE STRINGS.
+
+  switch (action.type) {
+    case REQUEST_USER_DATA + "_FULFILLED":
+      const { email, firstName, lastName } = action.payload.user;
+      return { email, firstName, lastName };
+    default:
+      return state;
+  }
 }
